c5b94cd6d649e20676ec5fce7df398387f7937b2
PrateekTechmatys <prateeky562@gmail.com>
2024-02-26 14:43:05 +0530

    First_div

---
+++ index.js
@@ -0,0 +1,32 @@
+
+//  1 way to go this
+const check =document.getElementById('check');
+
+check.style.backgroundColor="yellow";
+check.style.margin="20px";
+check.style.padding="10px";
+check.style.fontSize="18px";
+check.style.fontWeight="bold";
+check.style.height="200px";
+check.style.width="300px";
+
+
+
+
+
+// 2 way to do this 
+// const check = document.getElementById('check');
+
+// function applyStyles(element, styles) {
+//     Object.assign(element.style, styles);
+// }
+
+// applyStyles(check, {
+//     backgroundColor: "yellow",
+//     margin: "20px",
+//     padding: "10px",
+//     fontSize: "18px",
+//     fontWeight: "bold",
+//     height: "200px",
+//     width: "300px"
+// });
